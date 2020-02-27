796d1a88c121a599029e1e098db807c8d59e1d77
Rodriguez fennema lucas <ld.fennema@gmail.com>
2020-02-27 16:31:08 -0300

    mongoose conect change

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.use("/board", boardRouter);
 app.use("/cbm", bmrouter);
 if (process.env.environment !== "test") {
   const uri = process.env.URIMONGO;
-  mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("conected 2 db"))
-    .catch(e => console.log(e));
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  mongoose.connection.on("connected", () => {
+    console.log("bd conected");
+  });
   let port = process.env.PORT || 3000;
   app.listen(port, () => console.log("server started"));
 }
