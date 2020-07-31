c2b29528db587409e1145e72b95d03ce426c4255
Fennema lucas <ld.fennema@gmail.com>
2020-07-30 23:29:02 -0300

    catch promise err

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -9,19 +9,22 @@ const transporter  =   nodemailer.createTransport({
     pass:  process.env.PASSWORDMAIL,
   }
 })
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   let {name, mail, message} = req.body;
   
-  let info = await transporter.sendMail({
+  transporter.sendMail({
     from: process.env.USERMAIL,
     to: process.env.MAILTO, 
     subject: "My site contact "+ name +" respond to: "+ mail, 
     text:"from: "+name+" replay to: "+mail +" message: "+ message,
-  });
-  if(info.response.split(' ')[2] === 'OK' )
+  }).then(res  => {
+    if (info.response.split(' ')[2] === 'OK' )
     res.status(200).json({message:"se envio con exito"})
   else
     res.status(400).json({message:"lo sentimos no se pudo enviar el mail devido a un error en el servicio"})
-  });
+  }).catch(err  => {
+    res.status(400).json({message: err})
+  })
+})
 
 module.exports = router; 
